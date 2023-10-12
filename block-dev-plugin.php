a55cf82115a91e7f3493ee3b5bafe1762d4f6a39
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-12 20:33:35 -0300

    init

---
+++ block-dev-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Block Dev Plugin
+ * Description:       Desenvolvimento de Plugin utilizando React | blocos personalizados.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jonathan Santos
+ * Text Domain:       block-dev-plugin
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
