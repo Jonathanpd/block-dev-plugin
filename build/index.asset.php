5f04c12abe5a68835636934ee06d3e5837ac2e5a
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-12 21:47:21 -0300

    feat: creating a block metadata file | bundle | build

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '118e7f3c11580fe9b855');
---
+++ build/index.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('test');
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
